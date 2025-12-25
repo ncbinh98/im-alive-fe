421055dc3f0f69c790c90d4656793a89668102a3
Blue Ace <deadtoy0101@gmail.com>
2025-12-25 08:20:59 +0700

    feat(alive-8): Init nextJS source

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+  console.log("im clients");
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ components/hello.tsx
@@ -0,0 +1,11 @@
+"use client";
+const Hello = () => {
+  console.log("im clients");
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
+};
+
+export default Hello;
